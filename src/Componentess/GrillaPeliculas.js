77514b9e53902bea50842c9f0df8a5c717d4d20e
Laura <h.arcelaura@gmail.com>
2019-05-29 22:26:16 -0300

    Update GrillaPeliculas.js

--- src/Componentess/GrillaPeliculas.js
+++ src/Componentess/GrillaPeliculas.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import {Container,Row,Col,Button,Card} from 'react-bootstrap';
+import { Grid, Card, CardActionArea, CardContent, CardMedia, Typography, CardActions, Button } from '@material-ui/core';
 
 const styles = theme => ({
   root: {
@@ -36,24 +36,34 @@ function GrillaPeliculas(props) {
   return (
 
     <div className={classes.root}>
-      
+      <Grid Container className={classes.gridList}>
       {tileData.map(tile => (
-        <Card>
-  <Card.Img variant="top" src={tile.img} />
-  <Card.Body>
-    <Card.Title>{tile.title}</Card.Title>
-    <Card.Text>
-      {tile.author}
-    </Card.Text>
-  </Card.Body>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+        <Card className={classes.card}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              imagen={tile.img}
+              title={tile.title}
+              author={tile.author}
+            />
+          </CardActionArea>
+         {/* <Card.Img variant="top" src={tile.img} /> */}
+            {/* <Card.Body> */}
+          {/* <Card.Title>{tile.title}</Card.Title> */}
+            {/* <Card.Text> */}
+      {/* {tile.author} */}
+    {/* </Card.Text> */}
+            {/* </Card.Body> */}
   
-  <Card.Body>
-    <Button href="#" style={{margin:'10px'}}>Calificar</Button>
-    <Button href="#">Comentarios</Button>
-  </Card.Body>
-</Card>
+              <CardActionArea>
+                 <Button size='small' href="#" style={{margin:'10px'}}>Calificar</Button>
+                <Button size='small' href="#">Comentarios</Button>
+              </CardActionArea>
+          </Card>
+          </Grid>
         ))}
-      
+      </Grid>
 
       </div>
     
